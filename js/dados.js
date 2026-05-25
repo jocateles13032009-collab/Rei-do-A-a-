6d991b9cde226e89919f4d8ef14385b4a6675872
jocateles13032009-collab <jocateles13032009@gmail.com>
2026-05-25 15:56:20 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,66 @@
+ITENS = [
+  {
+    id: 1,
+    nome: "Açaí 300ml",
+    descricao: "2 acompanhamentos inclusos",
+    preco: 10,
+    imagem: "img/img-1.png"
+  },
+
+  {
+    id: 2,
+    nome: "Açaí 500ml",
+    descricao: "4 acompanhamentos inclusos",
+    preco: 15,
+    imagem: "img/img-2.png"
+  },
+
+  {
+    id: 3,
+    nome: "Açaí 700ml",
+    descricao: "6 acompanhamentos inclusos",
+    preco: 20,
+    imagem: "img/img-3.png"
+  },
+
+  {
+    id: 4,
+    nome: "Açaí Especial",
+    descricao: "Nutella + Morango + Leite condensado",
+    preco: 25,
+    imagem: "img/img-4.png"
+  },
+
+  {
+    id: 5,
+    nome: "Milkshake de Açaí",
+    descricao: "Milkshake cremoso de açaí",
+    preco: 18,
+    imagem: "img/img-5.png"
+  },
+
+  {
+    id: 6,
+    nome: "Barca de Açaí",
+    descricao: "Açaí gigante para compartilhar",
+    preco: 45,
+    imagem: "img/img-6.jpg"
+  },
+
+  {
+    id: 7,
+    nome: "Vitamina Tropical",
+    descricao: "Banana, morango e açaí",
+    preco: 14,
+    imagem: "img/img-7.jpg"
+  },
+
+  {
+    id: 8,
+    nome: "Combo Casal",
+    descricao: "2 açaís 500ml + toppings",
+    preco: 35,
+    imagem: "img/img-8.png"
+  }
+];
+
